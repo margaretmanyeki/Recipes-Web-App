6e23ffeeeff352efe678b92b1204e97dee2cf5d6
margaret manyeki <maggiemanyeki6@gmail.com>
2022-07-27 19:33:39 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Meal from "./Components/Meal";
 import{Routes,Route} from "react-router-dom"
+import RecipeInfo from "./Components/RecipeInfo";
 
 function App(){
     return(
         <>
        < Routes>
         <Route path="/" element={<Meal/>}/>
+        <Route path="/:MealId" element= {<RecipeInfo/>}/>
         </Routes>
         </>
     )
